abeca3b7aa63a5f5968c0b280473e8e035676468
Simon Tao <13thsymphony@gmail.com>
2020-06-16 22:23:18 -0700

    Minimal changes needed to compile for UWP (/zw)

--- src/libraw_datastream.cpp
+++ src/libraw_datastream.cpp
@@ -647,8 +647,7 @@ void *LibRaw_bigfile_datastream::make_jas_stream()
 LibRaw_windows_datastream::LibRaw_windows_datastream(const TCHAR *sFile)
     : LibRaw_buffer_datastream(NULL, 0), hMap_(0), pView_(NULL)
 {
-  HANDLE hFile = CreateFile(sFile, GENERIC_READ, 0, 0, OPEN_EXISTING,
-                            FILE_ATTRIBUTE_NORMAL, 0);
+  HANDLE hFile = CreateFile2(sFile, GENERIC_READ, 0, OPEN_EXISTING, 0);
   if (hFile == INVALID_HANDLE_VALUE)
     throw std::runtime_error("failed to open the file");
 
